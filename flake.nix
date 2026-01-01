d3e9d4c82fefbfec722c0e6697a6dfed23f77915
Qubic <77882752+AwesomeQubic@users.noreply.github.com>
2026-01-01 15:03:33 +0100

    add nix support

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Build a cargo project without extra checks";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    crane.url = "github:ipetkov/crane";
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      crane,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        craneLib = crane.mkLib pkgs;
+      in
+      {
+
+        devShells.default = craneLib.devShell {
+          packages = [];
+        };
+      }
+    );
+}
